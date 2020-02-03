dd413d36f9ae116fa3c87e36112375a5bf444587
tjchaill <tjchaill@iu.edu>
2020-02-03 17:57:37 -0500

    Create main.py

---
+++ text-adventure/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sys, os, json
+# Check to make sure we are running the correct version of Python
+assert sys.version_info >= (3,7), "This script requires at least Python 3.7"
+
+# The game and item description files (in the same folder as this script)
+game_file = 'zork.json'
+item_file = 'items.json'
+
+
+# Load the contents of the files into the game and items dictionaries. You can largely ignore this
+# Sorry it's messy, I'm trying to account for any potential craziness with the file location
+def load_files():
+    try:
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(__location__, game_file)) as json_file: game = json.load(json_file)
+        with open(os.path.join(__location__, item_file)) as json_file: items = json.load(json_file)
+        return (game,items)
+    except:
+        print("There was a problem reading either the game or item file.")
+        os._exit(1)
+
+# The main function for the game
+def main():
+    current = 'WHOUS'  # The starting location
+    end_game = ['END']  # Any of the end-game locations
+
+    (game,items) = load_files()
+
+    # Add your code here
+
+# run the main function
+if __name__ == '__main__':
+	main()
